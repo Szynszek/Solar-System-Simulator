637d738c2c2ce9b7de00523f73c63df5ab368e39
Szymon <64605003+Szynszek@users.noreply.github.com>
2026-03-14 18:40:34 +0100

    Translate comments and variables to English

--- orbita.m
+++ orbita.m
@@ -1,39 +1,39 @@
-%% Inicjalizacja
+%% Initialization
 clc
 close all
 clear
 
-%% Parametry fizyczne
-G = 6.674e-11; % [N m^2/kg^2] Stała grawitacji
+%% Physical parameters
+G = 6.674e-11; % [N m^2/kg^2] Gravitational Constant
 
-planety = {'Słońce', 'Ziemia', 'Mars'};
-masy_planet = [1.989e30, 5.972e24, 6.39e23]; % [kg] Masy Ciał
+bodies = {'Sun', 'Earth', 'Mars'};
+body_masses = [1.989e30, 5.972e24, 6.39e23]; % [kg] Body masses
 
-%% Stan początkowy
-r0_slonce = [0, 0, 0]; % [m] Początkowe położenie Słońca
-v0_slonce = [0, 0, 0]; % [m/s] Początkowa prędkość Słońca
-y0_slonce = [r0_slonce, v0_slonce]; % Wektor stanu początkowego Słońca
+%% Initial state
+r0_sun = [0, 0, 0]; % [m] Initial Sun position
+v0_sun = [0, 0, 0]; % [m/s] Initial Sun velocity
+y0_sun = [r0_sun, v0_sun]; % Initial Sun orbital state vector
 
-r0_ziemia = [0, 1.498e11, 0]; % [m] Początkowe położenie Ziemi
-v0_ziemia = [-29.78e3 * 0.70, 0, 0]; % [m/s] Początkowa prędkość Ziemi
-y0_ziemia = [r0_ziemia, v0_ziemia]; % Wektor stanu początkowego Ziemi
+r0_earth = [0, 1.498e11, 0]; % [m] Initial Earth position
+v0_earth = [-29.78e3 * 0.70, 0, 0]; % [m/s] Initial Earth velocity
+y0_earth = [r0_earth, v0_earth]; % Initial Earth orbital state vector
 
-r0_mars = [0, 2.279e11, 0]; % [m] Początkowe położenie Marsa
-v0_mars = [-24.07e3, 0, 0];% [m/s] Początkowa prędkość Marsa 
-y0_mars = [r0_mars, v0_mars]; % Wektor stanu początkowego Marsa
+r0_mars = [0, 2.279e11, 0]; % [m] Initial Mars position
+v0_mars = [-24.07e3, 0, 0];% [m/s] Initial Mars velocity 
+y0_mars = [r0_mars, v0_mars]; % Initial Mars orbital state vector
 
-y0 = [y0_slonce, y0_ziemia, y0_mars];
+y0 = [y0_sun, y0_earth, y0_mars];
 
-%% Ustawienia symulacji
-t_end = 31557600*2; % [s] Czas trwania symulacji
-t_span = [0, t_end]; % [s] Przedział czasu symulacji
+%% Simulation configuration
+t_end = 31557600*2; % [s] Simulation duration
+t_span = [0, t_end]; % [s] Simulation time span
 
-%% Obliczenia numeryczne
-uchwyt_fizyki = @(t,y) oblicz_pochodne(t, y, G, masy_planet);
-opcje = odeset('RelTol', 1e-9, 'AbsTol', 1e-9);
-[t_out, y_out] = ode45(uchwyt_fizyki, t_span, y0, opcje);
+%% Numerical calculations
+physics_handle = @(t,y) calc_derivative(t, y, G, body_masses);
+options = odeset('RelTol', 1e-9, 'AbsTol', 1e-9);
+[t_out, y_out] = ode45(physics_handle, t_span, y0, options);
 
-%% Wizualizacja
+%% Visualization
 X = y_out(:, 1:6:end);
 Y = y_out(:, 2:6:end);
 Z = y_out(:, 3:6:end);
@@ -41,19 +41,19 @@ Z = y_out(:, 3:6:end);
 figure
 hold on
 plot3(X, Y, Z, 'LineWidth', 1.5)
-legend(planety)
+legend(bodies)
 axis equal
 grid on
-xlabel('Pozycja X [m]')
-ylabel('Pozycja Y [m]')
-zlabel('Pozycja Z [m]')
-title('Trajektoria orbitalna')
+xlabel('Position X [m]')
+ylabel('Position Y [m]')
+zlabel('Position Z [m]')
+title('Orbital Trajectory')
 hold off
 
 
-%% Funkcje
-function dydt = oblicz_pochodne(t, y, G, masy_planet)
-    N = length(masy_planet);
+%% Local functions
+function dydt = calc_derivative(t, y, G, body_masses)
+    N = length(body_masses);
     y_formated = reshape(y, 6, N);
     r = y_formated(1:3,:);
     v = y_formated(4:6,:);
@@ -62,7 +62,7 @@ function dydt = oblicz_pochodne(t, y, G, masy_planet)
     dr = r_j-r_i;
     dist = vecnorm(dr,2,1);
     dist(dist == 0) = inf;
-    m_3d = reshape(masy_planet, 1, 1, N);
+    m_3d = reshape(body_masses, 1, 1, N);
     da = G .* m_3d .*dr ./dist.^3;
     a = sum(da,3);
 
